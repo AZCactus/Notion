4078c5572a63aa5c110b5289eadc2f060238ec2a
Hal Carleton <hello@halleycarleton.com>
2017-02-09 16:27:57 -0600

    Update comment.js

--- client/actions/comment.js
+++ client/actions/comment.js
@@ -15,7 +15,6 @@ export const addNewComment = (comment, noteId) => ({
 });
 
 export const createComment = (text, noteId, userId) => {
-  console.log('USER ID', userId);
   return dispatch => {
     return axios.post('/api/comment/', {text, noteId, userId})
       .then((res) => res.data)
